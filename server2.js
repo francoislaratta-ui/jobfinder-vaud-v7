49895b8ee2b7a09da757cfcc48cb01674969fcc3
francoislaratta-ui <francois.laratta@gmail.com>
2026-05-29 23:59:30 +0200

    Update server2.js

--- server2.js
+++ server2.js
@@ -1,18 +1,16 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "ok",
-    app: "Job Finder Vaud V7",
-    version: "1.0"
-  });
-});
 app.get("/offers", (req, res) => {
   const data = require("./offers.json");
   res.json(data);
+});
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
