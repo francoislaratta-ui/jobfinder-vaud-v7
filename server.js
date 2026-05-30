d8bf009c0013c534b70b32b676e0301163282347
francoislaratta-ui <francois.laratta@gmail.com>
2026-05-30 13:33:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/offers", (req, res) => {
+  const data = require("./offers.json");
+  res.json(data);
+});
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
