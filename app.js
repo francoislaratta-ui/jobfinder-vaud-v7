b34833c8b78ce04922ad14e3fe2c85af69b7c90e
francoislaratta-ui <francois.laratta@gmail.com>
2026-06-03 23:27:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+/* ==========================================
+JOB FINDER VAUD V14.0.0 PREMIUM IA
+Créateur : F. Laratta
+========================================== */
 
+/* ==========================================
+STORAGE
+========================================== */
+
+const STORAGE_KEYS = {
+
+favorites: "jobfinder_favorites",
+
+applications: "jobfinder_applications",
+
+settings: "jobfinder_settings",
+
+stats: "jobfinder_stats",
+
+offers: "jobfinder_offers"
+
+};
+
+/* ==========================================
